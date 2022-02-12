929a79e3472326dc7f076e98962e48077baca2f3
조가린 <41724184+gareen9342@users.noreply.github.com>
2022-02-12 15:24:11 +0900

    refactor test code

--- test/kakao.test.spec.ts
+++ test/kakao.test.spec.ts
@@ -2,8 +2,19 @@ import supertest from 'supertest'
 import assert from "assert"
 import app from '../app'
 
-
 const req = supertest(app)
+
+const requestPost = (uri: string, done) => {
+  req.post(uri)
+      .expect(200, (err, res) => {
+        if (err) {
+          done(err)
+        } else {
+          assert(res.body.resultCode === 0)
+          done()
+        }
+      })
+}
 
 describe('health check', () => {
 
@@ -19,33 +30,10 @@ describe('health check', () => {
 })
 
 describe("친구 목록 받아오기", () => {
-  it("친구 목록 받아오기 체크", (done) => {
-
-    req.post("/kakao/friends")
-        .expect(200, (err, res) => {
-          if (err) {
-            done(err)
-          } else {
-            // console.log(res.body.resultCode)
-            // TODO : assert typing
-            assert(res.body.resultCode === 0)
-            done()
-          }
-        })
-  })
+  it("친구 목록 받아오기 체크", (done) => requestPost("/kakao/friends", done))
 })
 
 describe("메세지 보내기", () => {
-  it("정상작동 테스트", (done) => {
-    req.post("/message")
-        .expect(200, (err, res) => {
-          if (err) {
-            done(err)
-          } else {
-            assert(res.body.resultCode === 0)
-            done()
-          }
-        })
-  })
-
+  it("정상작동 테스트", (done) => requestPost("/messages", done))
 })
+
